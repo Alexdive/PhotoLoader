3b763eaadbfb8b5c1150867c98a5cb4de307b85e
Alex Permiakov <alextddc@gmail.com>
2021-01-22 17:35:12 +0800

    Initial Commit

---
+++ PhotoLoader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoLoader
+//
+//  Created by Alex Permiakov on 1/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
