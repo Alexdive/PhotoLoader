e009f141e770bd67b055a4d210d54b675f4d5474
Alex Permiakov <alextddc@gmail.com>
2021-01-30 12:48:37 +0800

    timer added

--- PhotoLoader/ViewController.swift
+++ PhotoLoader/ViewController.swift
@@ -11,6 +11,15 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    let countLabel = UILabel()
+    
+    var counter = 0 {
+        didSet {
+            let min = counter / 60
+            countLabel.text = "is open for \(min) min \(counter - min * 60) sec"
+        }
+    }
+    
     lazy var photoCV: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
@@ -45,26 +54,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setViews()
+        setTimer()
         loadListOfImages()
     }
     
     func loadListOfImages() {
         
         guard let url = URL(string: urlString) else { return }
-
+        
         URLSession.shared.dataTask(with: url) {
             data, _, error in
-
+            
             if error != nil
             {
                 print("error=\(String(describing: error))")
                 return
             }
-
+            
             guard let data = data else { return }
             do {
                 if let convertedJsonIntoArray = try JSONSerialization.jsonObject(with: data, options: []) as? NSArray {
-
+                    
                     self.images = convertedJsonIntoArray as [AnyObject]
                 }
             } catch let error as NSError {
@@ -73,16 +83,37 @@ class ViewController: UIViewController {
         }.resume()
     }
     
+    private func setTimer() {
+        let timer = Timer(timeInterval: 1.0,
+                          target: self,
+                          selector: #selector(fireTimer),
+                          userInfo: nil,
+                          repeats: true)
+        RunLoop.current.add(timer, forMode: .common)
+    }
+    
+    @objc func fireTimer() {
+        counter += 1
+    }
+    
     private func setViews() {
         
         title = "Photo Loader"
         view.backgroundColor = .systemGray
         
         view.addSubview(photoCV)
-        photoCV.addSubview(activityIndicator)
+        view.addSubview(countLabel)
+        view.addSubview(activityIndicator)
+        
+        countLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.centerX.equalToSuperview()
+        }
         
         photoCV.snp.makeConstraints { (make) in
-            make.edges.equalTo(view.safeAreaLayoutGuide.snp.edges)
+            make.top.equalTo(countLabel.snp.bottom).offset(16)
+            make.left.right.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
         activityIndicator.snp.makeConstraints { (make) in
             make.center.equalToSuperview()
@@ -125,7 +156,7 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         
-        return UIEdgeInsets(top: baseInset, left: baseInset, bottom: baseInset, right: baseInset)
+        return UIEdgeInsets(top: 0, left: baseInset, bottom: 0, right: baseInset)
     }
     
     private func widthForSection(_ collectionView: UICollectionView, numberOfItems: CGFloat, inset: CGFloat) -> CGFloat {
